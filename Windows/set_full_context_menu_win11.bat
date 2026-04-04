ef791081d3b61fb6a67a6eb2768292714066a94a
Nico <thealdobaldo@gmail.com>
2026-04-04 14:14:48 +0200

    up: updated script

--- Windows/set_full_context_menu_win11.bat
+++ Windows/set_full_context_menu_win11.bat
@@ -1,17 +1,11 @@
 @echo off
 
-REM Must be run as Administrator
+REM from: https://askleo.com/restore-the-right-click-menu-in-windows-11/
 
-REM Enable classic context menus in Explorer
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\FlightedFeatures" /v ImmersiveContextMenu /t REG_DWORD /d 0 /f
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
-REM Revert back to immersive context menus
-REM reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\FlightedFeatures" /v ImmersiveContextMenu /f
-
-REM Restart Explorer to apply changes immediately
-echo Restarting Explorer to apply changes...
-taskkill /f /im explorer.exe
-start explorer.exe
+REM To restore default Windows 11 context menu:
+REM reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 echo.
 echo Immersive context menu status updated in Explorer.
